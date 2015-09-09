7041ef5d46f915f7ec7eb9bcb864ae2b298a55bb
Holden Link <holden@holdenlink.com>
2015-09-09 11:10:59 -0700

    Initial commit of TButt files

---
+++ Assets/TButt/Scripts/Utilities/TBCullByPlatform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using TButt;
+
+namespace TButt
+{
+    /// <summary>
+    /// Destroys GameObjects that shouldn't be used on particular platforms.
+    ///  
+    /// USAGE: 
+    /// 1. Set this script early on Unity's script execution order.
+    /// 2. Add this component to a GameObject in the scene or a prefab.
+    /// 3. Set the lowestAllowed and highestAllowed platforms via the editor.
+    /// 4. Make sure TBPlatformFilter is getting initialized by one of your manager scripts earlier in the script execution order.
+    /// 
+    /// Lowest and highest allowed can be the same platform, if you wish to only have that object support on one platform.
+    /// 
+    /// Original Version 09/09 2015 by Turbo Button
+    /// 
+    /// </summary>
+	public class TBCullByPlatform : MonoBehaviour {
+
+		public TBQualityLevel lowestAllowed;
+		public TBQualityLevel highestAllowed;
+
+		private int qualityLevel;
+
+		void Awake () 
+		{
+            qualityLevel = (int)TBPlatformFilter.GetQualityLevel();
+
+			if(qualityLevel < (int)lowestAllowed)
+			{
+                DestroyImmediate(gameObject);
+			}
+			else if(qualityLevel > (int)highestAllowed)
+			{
+				DestroyImmediate(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/TButt/Scripts/Utilities/TBPlatformFilter.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TButt
+{
+    /// <summary>
+    /// A simple manager for toggling features on different VR platforms.
+    /// 
+    /// Out-of-the-box support for Gear VR Note 4, Gear VR S6, PS4, and PC
+    /// 
+    /// USAGE: 
+    /// 1. Call TBPlatformFilter.SetQualitySettiongs() from any script that runs at startup.
+    /// 2. Use TBPlatformFilter.GetQualityLevel() to access the quality level from anywhere, and change your settings based on the 
+    /// TBQuality level enum returned.
+    /// 
+    /// The "TBCullByPlatform" script will let you quickly turn off GameObjects on certain platforms. Set lowestAllowed and highestAllowed
+    /// in the editor on each scene object or prefab that you want to toggle. See that script for more usage information.
+    /// 
+    /// OPTIONAL:
+    /// Set up quality settings in Unity for each platform as necessary. Example settings are inline below. Uncomment the
+    /// QualitySettings.SetQualityLevel() lines to use that feature.
+    /// 
+    /// Original Version 09/09 2015 by Turbo Button
+    /// 
+    /// </summary>
+    public static class TBPlatformFilter 
+    {
+        private static TBQualityLevel quality; // do not delete this
+
+	    public static void SetQualitySettings () 
+        {
+            // Gear VR Settings
+            #if UNITY_ANDROID
+                string gpu = SystemInfo.graphicsDeviceName;
+                if ((gpu == "Adreno (TM) 420"))
+                {
+                    // Snapdragon Galaxy Note 4 Settings
+                    quality = TBQualityLevel.MobileLow;
+                    //QualitySettings.SetQualityLevel(0); 
+                }
+                else
+                {
+                    // Galaxy S6 (or better) Settings
+                    quality = TBQualityLevel.MobileHigh;
+                    //QualitySettings.SetQualityLevel(1);
+                }
+            #endif
+
+            // PS4 Morpheus Settings
+            #if UNITY_PS4
+                quality = TBQualityLevel.QualityLevel.PCLow;
+                //QualitySettings.SetQualityLevel(2);
+            #endif
+
+            // PC Settings
+            #if UNITY_STANDALONE
+                quality = TBQualityLevel.PC;
+                //QualitySettings.SetQualityLevel(3);
+            #endif
+	    }
+
+        public static TBQualityLevel GetQualityLevel()
+        {
+            return quality;
+        }
+    }
+}
+
+public enum TBQualityLevel
+{
+        MobileLow,
+        MobileHigh,
+        PS4,
+        PC
+}
